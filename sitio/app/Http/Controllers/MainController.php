9deab23e4072c737e9bed17210afea1b94396ce5
Hernan Arregoces <harregoces@gmail.com>
2017-10-01 20:43:43 -0500

    GA y GTM install

---
+++ sitio/app/Http/Controllers/MainController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use App\task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MainController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function index(Request $request, $idcliente)
+    {
+        //check if the user has the GTM installed
+        $task = DB::table('tasks')->where('idcliente',$idcliente)->first();
+
+        $response = new \Illuminate\Http\Response(view('welcome')->with('task',$task));
+        $response->withCookie(cookie('idcliente', $idcliente, 450000));
+        return $response;
+    }
+
+    public function installPlugingtm(Request $request)
+    {
+        $google_client = new \Google_Client();
+        $google_client->setApplicationName('Web client OAuth');
+        $google_client->setClientId('349982058915-lqccda1kbqmdstrn6nm50b0qdhk8pr2q.apps.googleusercontent.com');
+        $google_client->setClientSecret('-bFJBmY-EhckJDplREV33vU9');
+        $google_client->setRedirectUri('http://'.$_SERVER['HTTP_HOST'].'/installplugincallbackgtm/');
+        $google_client->addScope(\Google_Service_TagManager::TAGMANAGER_READONLY);
+
+        $auth_url = $google_client->createAuthUrl();
+        header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+        exit;
+    }
+
+    public function installPluginga(Request $request)
+    {
+        $google_client = new \Google_Client();
+        $google_client->setApplicationName('Web client OAuth');
+        $google_client->setClientId('349982058915-lqccda1kbqmdstrn6nm50b0qdhk8pr2q.apps.googleusercontent.com');
+        $google_client->setClientSecret('-bFJBmY-EhckJDplREV33vU9');
+        $google_client->setRedirectUri('http://'.$_SERVER['HTTP_HOST'].'/installplugincallbackga/');
+        $google_client->addScope(\Google_Service_Analytics::ANALYTICS_READONLY);
+
+        $auth_url = $google_client->createAuthUrl();
+        header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+        exit;
+    }
+
+    public function callbackPlugingtm(Request $request) {
+        $code = $_GET['code'];
+        $idcliente = $request->cookie('idcliente');
+        $gtm_code = $code;
+
+        $task = DB::table('tasks')->where('idcliente',$idcliente)->first();
+
+        if(!$task) {
+            DB::insert('insert into tasks (idcliente,gtm_code) values (?, ?)', [ $idcliente, $gtm_code]);
+        } else {
+            DB::update('update tasks set gtm_code = ? where idcliente = ?', [$gtm_code,$idcliente]);
+        }
+
+        return \Redirect::to('/idcliente/'.$idcliente);
+    }
+
+    public function callbackPluginga(Request $request) {
+        $code = $_GET['code'];
+        $idcliente = $request->cookie('idcliente');
+        $ga_code = $code;
+
+        $task = DB::table('tasks')->where('idcliente',$idcliente)->first();
+
+        if(!$task) {
+            DB::insert('insert into tasks (idcliente,ga_code) values (?, ?)', [$idcliente, $ga_code]);
+        } else {
+            DB::update('update tasks set ga_code = ? where idcliente = ?', [$ga_code,$idcliente]);
+        }
+
+        return \Redirect::to('/idcliente/'.$idcliente);
+    }
+}
---
+++ sitio/resources/views/installplugin.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="{!! asset('public/css/bootstrap_css/bootstrap.min.css') !!}" />
+</head>
+<body>
+
+<div class="container">
+
+    install plugin
+
+</div>
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{ asset('public/js/jquery-3.2.1.slim.min.js') }}" ></script>
+<script src="{{ asset('public/js/popper.min.js') }}" ></script>
+<script src="{{ asset('public/js/bootstrap_js/bootstrap.min.js') }}" ></script>
+
+</body>
+
+</html>
