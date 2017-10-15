e9cbf3e67f395837114b0c30ddc170694b50f7bb
zggonzalez <sgonzalez@kooomo.com>
2017-10-16 00:06:26 +0100

    Third Party Visualizations

--- sitio/resources/views/dashboardhome.blade.php
+++ sitio/resources/views/dashboardhome.blade.php
@@ -3,4 +3,5 @@
 <h2>Basic: <a href="/dashboard/merchantid/{{$idcliente}}/type/basic">Basic</a></h2>
 <h2>Multiple: <a href="/dashboard/merchantid/{{$idcliente}}/type/multiple">Multiple</a></h2>
 <h2>Interactive: <a href="/dashboard/merchantid/{{$idcliente}}/type/interactive">Interactive</a></h2>
+<h2>Third Party: <a href="/dashboard/merchantid/{{$idcliente}}/type/thirdparty">Interactive</a></h2>
 </html>
